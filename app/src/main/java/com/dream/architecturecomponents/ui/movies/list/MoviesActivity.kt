072e8b9372db32b54c2a4bb38f77d9f042abf714
Benjamin Ledet <ledet.benjamin23@gmail.com>
2018-12-09 13:45:49 +0100

    add missing binding to moviesActivity

--- app/src/main/java/com/dream/architecturecomponents/ui/movies/list/MoviesActivity.kt
+++ app/src/main/java/com/dream/architecturecomponents/ui/movies/list/MoviesActivity.kt
@@ -2,20 +2,24 @@ package com.dream.architecturecomponents.ui.movies.list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.library.baseAdapters.BR
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dream.architecturecomponents.R
 import com.dream.architecturecomponents.data.Movie
+import com.dream.architecturecomponents.databinding.ActivityMoviesBinding
 import com.dream.architecturecomponents.extension.startAnimatedActivity
 import com.dream.architecturecomponents.ui.movies.create.CreateMovieActivity
 import com.dream.architecturecomponents.ui.movies.detail.DetailMovieActivity
-import kotlinx.android.synthetic.main.activity_movies.*
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MoviesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMoviesBinding
 
     private val viewModel: MoviesViewModel by lazy { ViewModelProviders.of(this).get(MoviesViewModel::class.java) }
 
@@ -23,7 +27,9 @@ class MoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movies)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movies)
+        binding.setVariable(BR.viewModel, viewModel)
+        binding.setLifecycleOwner(this)
 
         setupAdapter()
         setupFab()
@@ -42,11 +48,11 @@ class MoviesActivity : AppCompatActivity() {
     }
 
     private fun setupFab() {
-        fab.onClick { startAnimatedActivity(intentFor<CreateMovieActivity>()) }
+        binding.fab.onClick { startAnimatedActivity(intentFor<CreateMovieActivity>()) }
     }
 
     private fun setupRecyclerView() {
-        recyclerView.apply {
+        binding.recyclerView.apply {
             addItemDecoration(DividerItemDecoration(this@MoviesActivity, DividerItemDecoration.VERTICAL))
             layoutManager = LinearLayoutManager(this@MoviesActivity)
             adapter = moviesAdapter
