6e1aa28eba76c3b0192e72b2acf83447fe3663c8
Benjamin Ledet <ledet.benjamin23@gmail.com>
2018-12-22 16:35:00 +0100

    remove unused imports

--- app/src/main/java/com/dream/architecturecomponents/ui/movies/create/CreateMovieActivity.kt
+++ app/src/main/java/com/dream/architecturecomponents/ui/movies/create/CreateMovieActivity.kt
@@ -4,15 +4,10 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.library.baseAdapters.BR
-import androidx.lifecycle.ViewModelProviders
 import com.dream.architecturecomponents.R
 import com.dream.architecturecomponents.databinding.ActivityCreateMovieBinding
 import com.dream.architecturecomponents.ui.base.BaseActivity
-import com.dream.architecturecomponents.ui.movies.detail.DetailMovieViewModel
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.util.*
